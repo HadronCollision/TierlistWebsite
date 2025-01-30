ea990e3865fa563a1b51298be9bce3e07932fa63
Abdullah Zafar <abdullahzafarrana@gmail.com>
2025-01-30 18:51:52 +0500

    renamed colors and added a new one

--- src/constants/Color.js
+++ src/constants/Color.js
@@ -2,7 +2,8 @@ export const Color = {
   backgroundColor: "#111",
   headerColor: "#1C1C1C",
   textColor: "#ececec",
-  highTier: "#1e1e1e",
-  lowTier: "#292929",
+  primary: "#292929",
+  secondary: "#1e1e1e",
+  tertiary: "#555",
   loader: "#aaa",
 };
