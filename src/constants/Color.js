24f4f499dd10b98010a3441c6b4350cba7776037
Abdullah Zafar <abdullahzafarrana@gmail.com>
2025-01-30 18:57:52 +0500

    replaced primary and secondary colors

--- src/constants/Color.js
+++ src/constants/Color.js
@@ -2,8 +2,8 @@ export const Color = {
   backgroundColor: "#111",
   headerColor: "#1C1C1C",
   textColor: "#ececec",
-  primary: "#292929",
-  secondary: "#1e1e1e",
+  secondary: "#292929",
+  primary: "#1e1e1e",
   tertiary: "#555",
   loader: "#aaa",
 };
