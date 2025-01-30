d91bfe60ae38bbca38de3d02a30b9ee7c3b7602f
Abdullah Zafar <abdullahzafarrana@gmail.com>
2025-01-30 08:54:41 +0500

    removed unused import

--- src/components/modal/TierModal.jsx
+++ src/components/modal/TierModal.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Color } from "../../constants/color";
 import { useTierModal } from "../../hooks/useTierModal";
 import { BeatLoader } from "react-spinners";
