62455fd90948c4e6864d37ef7f68f0c6e44944c8
Abdullah Zafar <abdullahzafarrana@gmail.com>
2025-01-31 19:01:28 +0500

    installed stylex

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,44 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import stylex from "@stylexjs/eslint-plugin";
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ["dist"] },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,jsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
+        ecmaVersion: "latest",
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        sourceType: "module",
       },
     },
-    settings: { react: { version: '18.3' } },
+    settings: { react: { version: "18.3" } },
     plugins: {
       react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "@stylexjs": stylex,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
+      ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
+      "react/jsx-no-target-blank": "off",
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
+      "@stylexjs/valid-styles": "error",
+      "@stylexjs/no-unused": "error",
+      "@stylexjs/valid-shorthands": "warning",
+      "@stylexjs/sort-keys": "warning",
     },
   },
-]
+];
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { ngrok } from "vite-plugin-ngrok";
+import styleX from "vite-plugin-stylex";
 
 export default defineConfig({
   plugins: [
     react(),
+    styleX(),
     ngrok("2d7bdlaxnIl99xe2bI9RqxQBdaB_2TLLptmYG1rofLo3uVqgH"),
   ],
 });
