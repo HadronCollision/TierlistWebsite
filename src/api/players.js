67c13d62178003e2d75a9f8ab994ca5d85035282
Abdullah Zafar <abdullahzafarrana@gmail.com>
2025-01-31 21:36:15 +0500

    installed serve

--- src/api/players.js
+++ src/api/players.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
 export const fetchTierData = async (mode) => {
-  const { data } = await axios.get(`http://localhost:6969/api/tiers/${mode}`);
+  const { data } = await axios.get(
+    `https://tame-bananas-scream.loca.lt/api/tiers/${mode}`
+  );
   return data;
 };
 
 export const fetchPlayerData = async (ign) => {
   const { data } = await axios.get(
-    `http://localhost:6969/api/search?ign=${ign}`
+    `https://tame-bananas-scream.loca.lt/api/search?ign=${ign}`
   );
   return data;
 };
