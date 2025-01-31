3b9ae048b255a408f486e858c4398b1313ced20f
Abdullah Zafar <abdullahzafarrana@gmail.com>
2025-01-31 16:02:30 +0500

    moved font from div to css *

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
+import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import Tiers from "./pages/Tiers";
 import { ModalProvider } from "./context/modalContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -12,7 +12,6 @@ import AdminDashboard from "./pages/AdminDashboard";
 import Header from "./components/layout/Header";
 import Overall from "./pages/Overall";
 import Subhuman from "./pages/Subhuman";
-import Font from "react-font";
 
 const queryClient = new QueryClient();
 
@@ -23,31 +22,29 @@ createRoot(document.getElementById("root")).render(
         <SelectedModeProvider>
           <SearchProvider>
             <ModalProvider>
-              <Font family="Roboto">
-                <Routes>
-                  <Route element={<Header />}>
-                    <Route
-                      path="*"
-                      index
-                      element={<Navigate to="/ranking/overall" />}
-                    />
-                    <Route path="tiers">
-                      <Route index element={<Navigate to="/" />} />
-                      <Route path=":mode" element={<Tiers />} />
-                    </Route>
-                    <Route path="ranking">
-                      <Route index element={<Navigate to="/" />} />
-                      <Route path="overall" element={<Overall />} />
-                      <Route path="topten" element={<Overall />} />
-                      <Route path="subhuman" element={<Subhuman />} />
-                    </Route>
-                    <Route path="admin">
-                      <Route path="login" element={<AdminLogin />} />
-                      <Route path="dashboard" element={<AdminDashboard />} />
-                    </Route>
+              <Routes>
+                <Route element={<Header />}>
+                  <Route
+                    path="*"
+                    index
+                    element={<Navigate to="/ranking/overall" />}
+                  />
+                  <Route path="tiers">
+                    <Route index element={<Navigate to="/" />} />
+                    <Route path=":mode" element={<Tiers />} />
                   </Route>
-                </Routes>
-              </Font>
+                  <Route path="ranking">
+                    <Route index element={<Navigate to="/" />} />
+                    <Route path="overall" element={<Overall />} />
+                    <Route path="topten" element={<Overall />} />
+                    <Route path="subhuman" element={<Subhuman />} />
+                  </Route>
+                  <Route path="admin">
+                    <Route path="login" element={<AdminLogin />} />
+                    <Route path="dashboard" element={<AdminDashboard />} />
+                  </Route>
+                </Route>
+              </Routes>
             </ModalProvider>
           </SearchProvider>
         </SelectedModeProvider>
