a07f75301e47a3cdc96e9cd8430536905af9e4a2
Abdullah Zafar <abdullahzafarrana@gmail.com>
2025-02-01 14:51:01 +0500

    accidently pushed the wrong api url that I temporarily changed

--- src/api/players.js
+++ src/api/players.js
@@ -1,15 +1,13 @@
 import axios from "axios";
 
 export const fetchTierData = async (mode) => {
-  const { data } = await axios.get(
-    `https://tame-bananas-scream.loca.lt/api/tiers/${mode}`
-  );
+  const { data } = await axios.get(`http://localhost:6969/api/tiers/${mode}`);
   return data;
 };
 
 export const fetchPlayerData = async (ign) => {
   const { data } = await axios.get(
-    `https://tame-bananas-scream.loca.lt/api/search?ign=${ign}`
+    `http://localhost:6969/api/search?ign=${ign}`
   );
   return data;
 };
