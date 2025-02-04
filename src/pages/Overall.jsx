1b8aaf6d6b5afb47ec69e5e2f3288c4e9dc87f79
Abdullah Zafar <abdullahzafarrana@gmail.com>
2025-02-04 18:05:56 +0500

    added overall players to the page

--- src/pages/Overall.jsx
+++ src/pages/Overall.jsx
@@ -1,83 +1,156 @@
 import React, { useState } from "react";
+import Buttons from "../components/overall/Buttons";
 import * as stylex from "@stylexjs/stylex";
 import { colors } from "../tokens.stylex";
-import { motion } from "motion/react";
+import { domAnimation, LazyMotion } from "motion/react";
+import * as m from "motion/react-m";
+import { useModal } from "../context/modalContext";
 
 function Overall() {
   const [selectedButton, setSelectedButton] = useState("overall");
+  const { setModalState } = useModal();
+
   return (
-    <div {...stylex.props(styles.buttonContainer)}>
-      <div {...stylex.props(styles.buttonWrapper)}>
-        {["overall", "top10"].map((button) => (
-          <div
-            {...stylex.props(
-              selectedButton === button ? styles.buttonActive : styles.button
-            )}
-            onClick={() => setSelectedButton(button)}
-          >
-            {selectedButton === button && (
-              <motion.div
-                layoutId="underline"
-                {...stylex.props(styles.underline)}
-              ></motion.div>
-            )}
-            {button.toUpperCase()}
+    <div>
+      <Buttons
+        selectedButton={selectedButton}
+        setSelectedButton={setSelectedButton}
+      />
+      {selectedButton === "overall" && (
+        <div {...stylex.props(styles.container)}>
+          <div {...stylex.props(styles.subContainer)}>
+            <div {...stylex.props(styles.title)}>TOP 10 PAKISTAN</div>
+            <div {...stylex.props(styles.playerContainer)}>
+              {[
+                "FakeDrugLord123",
+                "YTMe_",
+                "ItzFearMe",
+                "xUltimate_",
+                "Raxizz",
+                "Ayan_X_Mn",
+                "FllNISH",
+                "xeob",
+                "StackeRrz",
+                "DrPuuuu",
+              ].map((player, i) => (
+                <LazyMotion features={domAnimation} key={i}>
+                  <m.span
+                    {...stylex.props(styles.playerCell)}
+                    whileTap={{ scale: 0.98 }}
+                    onClick={() =>
+                      setModalState({
+                        show: true,
+                        player: { ign: player, country: "pk" },
+                      })
+                    }
+                  >
+                    <span {...stylex.props(styles.index)}>{i + 1}</span>
+                    <img
+                      src={`https://render.crafty.gg/3d/head/${player}`}
+                      {...stylex.props(styles.skin)}
+                    />
+                    {player}
+                  </m.span>
+                </LazyMotion>
+              ))}
+            </div>
           </div>
-        ))}
-      </div>
+          <div {...stylex.props(styles.subContainer)}>
+            <div {...stylex.props(styles.title)}>TOP 10 INDIA</div>
+            <div {...stylex.props(styles.playerContainer)}>
+              {[
+                "PookieMall",
+                "Ywti",
+                "mistyibra",
+                "Swerin",
+                "xThunder___",
+                "ShubDaRizzler_",
+                "RunThe1s_",
+                "HYPERizOP",
+                "OhioKidooo",
+                "Sahibiguess",
+              ].map((player, i) => (
+                <LazyMotion features={domAnimation} key={i}>
+                  <m.span
+                    {...stylex.props(styles.playerCell)}
+                    whileTap={{ scale: 0.98 }}
+                    onClick={() =>
+                      setModalState({
+                        show: true,
+                        player: { ign: player, country: "in" },
+                      })
+                    }
+                  >
+                    <span {...stylex.props(styles.index)}>{i + 1}</span>
+                    <img
+                      src={`https://render.crafty.gg/3d/head/${player}`}
+                      {...stylex.props(styles.skin)}
+                    />
+                    {player}
+                  </m.span>
+                </LazyMotion>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
+      {selectedButton === "top 10" && <div></div>}
     </div>
   );
 }
 
 const styles = stylex.create({
-  buttonContainer: {
-    marginTop: "16px",
+  container: {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
     color: colors.textColor,
+    marginBottom: "32px",
   },
-  buttonWrapper: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
+  subContainer: {
     backgroundColor: colors.primary,
-    padding: "8px 0",
-    borderRadius: "25px",
+    padding: "12px",
+    borderRadius: "12px",
+    minWidth: "400px",
+    marginHorizontal: "1vw",
   },
-  button: {
-    position: "relative",
+  title: {
+    fontFamily: "Black Ops One",
+    fontSize: "24px",
+    textAlign: "center",
+  },
+  playerContainer: {
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+  },
+  playerCell: {
+    width: "100%",
+    height: "40px",
+    display: "flex",
+    alignItems: "center",
+    backgroundColor: colors.secondary,
+    margin: "4px 0px",
+    borderRadius: "4px",
+    fontSize: "20px",
+    cursor: "pointer",
+  },
+  index: {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "transparent",
-    width: "10vw",
-    padding: "2px 0",
-    marginHorizontal: "16px",
-    borderRadius: "8px",
-    fontFamily: "Audiowide",
-    cursor: "pointer",
-  },
-  buttonActive: {
-    position: "relative",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "transparent",
-    width: "10vw",
-    padding: "2px 0",
-    marginHorizontal: "16px",
-    borderRadius: "8px",
-    fontFamily: "Audiowide",
-    cursor: "pointer",
-  },
-  underline: {
-    position: "absolute",
-    left: "12px",
-    right: "12px",
     height: "100%",
-    border: `2px solid ${colors.tertiary}`,
-    borderRadius: "16px",
+    width: "32px",
+    borderRadius: "4px",
+    backgroundColor: colors.tertiary,
+  },
+  skin: {
+    height: "48px",
+    widht: "48px",
+  },
+  skinWrapper: {
+    height: "48px",
+    widht: "48px",
   },
 });
 
