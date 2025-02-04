27cfc083a570301d5b8405632f07554b6844651c
Abdullah Zafar <abdullahzafarrana@gmail.com>
2025-02-04 11:08:06 +0500

    added underline to GamemodeButtons

--- src/components/tiers/GamemodeButton.jsx
+++ src/components/tiers/GamemodeButton.jsx
@@ -1,7 +1,9 @@
-import React, { useCallback } from "react";
+import React from "react";
 import * as stylex from "@stylexjs/stylex";
 import { colors } from "../../tokens.stylex";
 import { NavLink } from "react-router";
+import { domAnimation, LazyMotion } from "motion/react";
+import * as m from "motion/react-m";
 
 function GamemodeButton({ gamemode, isSelected, onClick }) {
   console.log(gamemode.id);
@@ -17,13 +19,22 @@ function GamemodeButton({ gamemode, isSelected, onClick }) {
       >
         <img src={gamemode.src} {...stylex.props(styles.icon)} />
       </NavLink>
+      {isSelected && (
+        <LazyMotion features={domAnimation}>
+          <m.div
+            initial={{ y: 0, backgroundColor: colors.tertiary }}
+            animate={{ y: 4, backgroundColor: colors.loader }}
+            transition={{ duration: 0.3 }}
+            {...stylex.props(styles.underline)}
+          />
+        </LazyMotion>
+      )}
     </div>
   );
 }
 
 const styles = stylex.create({
   button: {
-    cursor: "pointer",
     textDecoration: "none",
     backgroundColor: colors.secondary,
     height: "48px",
@@ -33,17 +44,27 @@ const styles = stylex.create({
     alignItems: "center",
     borderRadius: "24px",
     fontSize: "18px",
+    cursor: "pointer",
   },
   buttonActive: {
     backgroundColor: colors.tertiary,
   },
   modalActive: {
     transform: "translateY(-3px)",
-    transition: "0.3s",
+    transition: "0.25s",
   },
   icon: {
     height: "80%",
     backgroundColor: "transparent",
+  },
+  underline: {
+    position: "absolute",
+    left: "20px",
+    right: "20px",
+    bottom: 0,
+    height: "2px",
+    borderRadius: "1px",
+    backgroundColor: colors.loader,
   },
 });
 
