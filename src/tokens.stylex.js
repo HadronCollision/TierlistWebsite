d5ea612bbb5187e68137b2f5f4e2e8a92e6f5d4b
Abdullah Zafar <abdullahzafarrana@gmail.com>
2025-02-09 18:17:20 +0500

    no change

--- src/tokens.stylex.js
+++ src/tokens.stylex.js
@@ -1,11 +1,11 @@
 import * as stylex from "@stylexjs/stylex";
 
 export const colors = stylex.defineVars({
-  backgroundColor: "#111",
-  headerColor: "#1c1c1c",
-  textColor: "#ececec",
-  secondary: "#292929",
-  primary: "#1e1e1e",
-  tertiary: "#525252",
-  loader: "#aaa",
+  backgroundColor: " #111111",
+  headerColor: " #1c1c1c",
+  textColor: " #ececec",
+  secondary: " #292929",
+  primary: " #1e1e1e",
+  tertiary: " #525252",
+  loader: " #aaaaaa",
 });
