6f40ce1bf77810cda993692438c4269cd0b6ccff
Abdullah Zafar <abdullahzafarrana@gmail.com>
2025-02-09 23:41:52 +0500

    refactored the routes

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,8 @@ import AdminDashboard from "./pages/AdminDashboard";
 import Header from "./components/layout/Header";
 import Leaderboard from "./pages/Leaderboard";
 import Subhuman from "./pages/Subhuman";
+import Top10LB from "./components/leaderboard/Top10LB";
+import GamemodeLeaderboard from "./components/leaderboard/GamemodeLeaderboard";
 
 const queryClient = new QueryClient();
 
@@ -27,17 +29,58 @@ createRoot(document.getElementById("root")).render(
                   <Route
                     path="*"
                     index
-                    element={<Navigate to="/ranking/leaderboard" />}
+                    element={<Navigate to="/leaderboard/overall" />}
                   />
+                  <Route path="leaderboard" element={<Leaderboard />}>
+                    <Route index element={<Navigate to="/" />} />
+                    <Route path="overall" element={<Top10LB />} />
+                    <Route
+                      path="pak_lb"
+                      element={
+                        <GamemodeLeaderboard
+                          country="pak"
+                          players={[
+                            { ign: "FakeDrugLord123", rank: "HT3" },
+                            { ign: "YTMe_", rank: "LT3" },
+                            { ign: "asimyuh_FAN", rank: "LT3" },
+                            { ign: "xUltimate_", rank: "LT3" },
+                            { ign: "Raxizz", rank: "LT3" },
+                            { ign: "Sqxshyy", rank: "LT3" },
+                            { ign: "FllNISH", rank: "LT3" },
+                            { ign: "xeob", rank: "LT3" },
+                            { ign: "StackeRrz", rank: "LT3" },
+                            { ign: "DrPuuuu", rank: "LT3" },
+                          ]}
+                        />
+                      }
+                    />
+                    <Route
+                      path="ind_lb"
+                      element={
+                        <GamemodeLeaderboard
+                          country="ind"
+                          players={[
+                            { ign: "360Mall", rank: "HT3" },
+                            { ign: "9fts", rank: "LT3" },
+                            { ign: "mistyibra", rank: "LT3" },
+                            { ign: "CattoL0VeR", rank: "LT3" },
+                            { ign: "Critspammer449", rank: "LT3" },
+                            { ign: "Critspammer449", rank: "LT3" },
+                            { ign: "ShubDaRizzler_", rank: "LT3" },
+                            { ign: "RunThe1s_", rank: "LT3" },
+                            { ign: "TimeIess_", rank: "LT3" },
+                            { ign: "OhioKidooo", rank: "LT3" },
+                            { ign: "Sahibiguess", rank: "LT3" },
+                          ]}
+                        />
+                      }
+                    />
+                  </Route>
                   <Route path="tiers">
                     <Route index element={<Navigate to="/" />} />
                     <Route path=":mode" element={<Tiers />} />
                   </Route>
-                  <Route path="ranking">
-                    <Route index element={<Navigate to="/" />} />
-                    <Route path="leaderboard" element={<Leaderboard />} />
-                    <Route path="subhuman" element={<Subhuman />} />
-                  </Route>
+                  <Route path="subhuman" element={<Subhuman />} />
                 </Route>
                 <Route path="admin">
                   <Route path="login" element={<AdminLogin />} />
