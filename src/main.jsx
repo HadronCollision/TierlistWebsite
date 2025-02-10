dfaa36c083b286cb833573ad7f394dca9945717e
Abdullah Zafar <abdullahzafarrana@gmail.com>
2025-02-10 20:48:51 +0500

    renamed Top10 to Overall and removed placeholder values for GamemodeLeaderboards

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import AdminDashboard from "./pages/AdminDashboard";
 import Header from "./components/layout/Header";
 import Leaderboard from "./pages/Leaderboard";
 import Subhuman from "./pages/Subhuman";
-import OverallLb from "./components/leaderboard/Top10LB";
+import OverallLb from "./components/leaderboard/OverallLb";
 import GamemodeLeaderboard from "./components/leaderboard/GamemodeLeaderboard";
 
 const queryClient = new QueryClient();
@@ -36,44 +36,11 @@ createRoot(document.getElementById("root")).render(
                     <Route path="overall" element={<OverallLb />} />
                     <Route
                       path="pak_lb"
-                      element={
-                        <GamemodeLeaderboard
-                          country="pak"
-                          players={[
-                            { ign: "FakeDrugLord123", rank: "HT3" },
-                            { ign: "YTMe_", rank: "LT3" },
-                            { ign: "asimyuh_FAN", rank: "LT3" },
-                            { ign: "xUltimate_", rank: "LT3" },
-                            { ign: "Raxizz", rank: "LT3" },
-                            { ign: "Sqxshyy", rank: "LT3" },
-                            { ign: "FllNISH", rank: "LT3" },
-                            { ign: "xeob", rank: "LT3" },
-                            { ign: "StackeRrz", rank: "LT3" },
-                            { ign: "DrPuuuu", rank: "LT3" },
-                          ]}
-                        />
-                      }
+                      element={<GamemodeLeaderboard country="pak" />}
                     />
                     <Route
                       path="ind_lb"
-                      element={
-                        <GamemodeLeaderboard
-                          country="ind"
-                          players={[
-                            { ign: "360Mall", rank: "HT3" },
-                            { ign: "9fts", rank: "LT3" },
-                            { ign: "mistyibra", rank: "LT3" },
-                            { ign: "CattoL0VeR", rank: "LT3" },
-                            { ign: "Critspammer449", rank: "LT3" },
-                            { ign: "Critspammer449", rank: "LT3" },
-                            { ign: "ShubDaRizzler_", rank: "LT3" },
-                            { ign: "RunThe1s_", rank: "LT3" },
-                            { ign: "TimeIess_", rank: "LT3" },
-                            { ign: "OhioKidooo", rank: "LT3" },
-                            { ign: "Sahibiguess", rank: "LT3" },
-                          ]}
-                        />
-                      }
+                      element={<GamemodeLeaderboard country="ind" />}
                     />
                   </Route>
                   <Route path="tiers">
